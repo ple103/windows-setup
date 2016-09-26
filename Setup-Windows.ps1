0b06575af9782efc5b69112eeaf36f9752556d02
Peter Le <peterhuyle@outlook.com>
2016-09-26 20:40:59 +1000

    Add code to disable UAC

--- Setup-Windows.ps1
+++ Setup-Windows.ps1
@@ -1,6 +1,9 @@
 # Set PowerShell as default
 New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name DontUsePowerShellOnWinX -PropertyType DWord -Value 0 -Force
 
+# Disable UAC
+New-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\policies\system -Name EnableLUA -PropertyType DWord -Value 0 -Force
+
 # Restart Windows Explorer
 Stop-Process Explorer
 Start-Process Explorer
