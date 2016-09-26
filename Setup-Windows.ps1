5ae1320f9d860820a211cf2a0068e8fdb7b5bbd3
Peter Le <peterhuyle@outlook.com>
2016-09-26 20:40:40 +1000

    Add code to set PowerShell as default console

--- Setup-Windows.ps1
+++ Setup-Windows.ps1
@@ -1,3 +1,10 @@
+# Set PowerShell as default
+New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name DontUsePowerShellOnWinX -PropertyType DWord -Value 0 -Force
+
+# Restart Windows Explorer
+Stop-Process Explorer
+Start-Process Explorer
+
 # Install Chocolatey
 iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
 
