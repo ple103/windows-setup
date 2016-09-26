83e5326409ffb1e3ed5a6982c27461a8af4bd9d0
Peter Le <peterhuyle@outlook.com>
2016-09-26 21:32:57 +1000

    Add code to show file extensions

--- Setup-Windows.ps1
+++ Setup-Windows.ps1
@@ -1,6 +1,9 @@
 # Set PowerShell as default
 New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name DontUsePowerShellOnWinX -PropertyType DWord -Value 0 -Force
 
+# Show file extensions
+New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name HideFileExt -PropertyType DWord -Value 0 -Force
+
 # Disable UAC
 New-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\policies\system -Name EnableLUA -PropertyType DWord -Value 0 -Force
 
