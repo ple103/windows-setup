7b9f3f7cfb3ebb9ce8ce52da1426694ddf3b64a2
Peter Le <peter.le@myriadtech.com.au>
2017-10-24 10:34:38 +1000

    Expand cmdlet aliases and move New-ItemProperty -Name to newline

--- Setup-Windows.ps1
+++ Setup-Windows.ps1
@@ -1,18 +1,21 @@
 # Set PowerShell as default
-New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name DontUsePowerShellOnWinX -PropertyType DWord -Value 0 -Force
+New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced `
+    -Name DontUsePowerShellOnWinX -PropertyType DWord -Value 0 -Force
 
 # Show file extensions
-New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name HideFileExt -PropertyType DWord -Value 0 -Force
+New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced `
+    -Name HideFileExt -PropertyType DWord -Value 0 -Force
 
 # Disable UAC
-New-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\policies\system -Name EnableLUA -PropertyType DWord -Value 0 -Force
+New-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\policies\system `
+    -Name EnableLUA -PropertyType DWord -Value 0 -Force
 
 # Restart Windows Explorer
 Stop-Process Explorer
 Start-Process Explorer
 
 # Install Chocolatey
-iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
 
 # Reload PowerShell
 . $profile
