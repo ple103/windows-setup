3e3bd113c9e8fc01fd86318223315a9964fc0934
Pete <peter.le@dpi.qld.gov.au>
2025-10-02 17:23:13 +1000

    create symlinks to profiles for PowerShell 5 & 7

--- step1.ps1
+++ step1.ps1
@@ -1,5 +1,8 @@
 # Written to be compatible with Windows PowerShell (5.1)
 
+Write-Warning -Message "Configure OneDrive backup before continuing"
+Start-Sleep -Milliseconds 1000
+
 if (-not
     # Current role
     (New-Object Security.Principal.WindowsPrincipal(
@@ -24,3 +27,32 @@ if (-not
 Write-Host "Installing WinGet PowerShell module from PSGallery.."
 Install-PackageProvider -Name NuGet -Force | Out-Null
 Install-Module -Name Microsoft.WinGet.Client -Force -Repository PSGallery | Out-Null
+
+$MyDocuments = [Environment]::GetFolderPath("MyDocuments")
+$PowerShell5ProfilePath = "$MyDocuments\WindowsPowerShell"
+$PowerShell7ProfilePath = "$MyDocuments\PowerShell"
+
+# Create symlink for Windows PowerShell 5.1 profile
+
+Write-Output "Creating symlink for PowerShell 5.1 profile"
+
+$params = @{
+    Path     = "$PowerShell5ProfilePath\Microsoft.PowerShell_profile.ps1"
+    ItemType = "SymbolicLink"
+    Value    = "$PSScriptRoot\Profiles\Microsoft.PowerShell5_profile.ps1"
+    Force    = $true
+}
+
+New-Item @params
+
+# Create symlink for PowerShell 7 profile
+Write-Output "Creating symlink for PowerShell 5.1 profile"
+
+$params = @{
+    Path     = "$PowerShell7ProfilePath\Microsoft.PowerShell_profile.ps1"
+    ItemType = "SymbolicLink"
+    Value    = "$PSScriptRoot\Profiles\Microsoft.PowerShell5_profile.ps1"
+    Force    = $true
+}
+
+New-Item @params
