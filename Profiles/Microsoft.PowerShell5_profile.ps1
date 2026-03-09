51f1f63bf4ca1f64a4ef84a5b16e86c5e9e7c647
Pete <peter.le@dpi.qld.gov.au>
2026-03-09 21:59:29 +1000

    refactor(powershell): move PowerShell profiles to dotfiles

--- Profiles/Microsoft.PowerShell5_profile.ps1
+++
@@ -1,5 +0,0 @@
-$ENV:STARSHIP_CONFIG = "$HOME\Source\Other\dotfiles\starship\starship.toml"
-$OutputEncoding = [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
-Invoke-Expression (&starship init powershell)
-Invoke-Expression (& { (zoxide init powershell | Out-String) })
-#Import-Module posh-git
