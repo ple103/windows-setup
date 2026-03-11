2bd188164832caff26251ce9268a431ab3147abc
Pete <peter.le@dpi.qld.gov.au>
2026-03-11 21:07:01 +1000

    feat: add Set-GitConfigSymbolicLinks script

---
+++ Set-GitConfigSymbolicLinks.ps1
@@ -0,0 +1,26 @@
+$NewItemParams = @{
+    Path     = ~/.gitconfig
+    ItemType = "SymbolicLink"
+    Value    = "$PSScriptRoot\dotfiles\git\.gitconfig"
+    Force    = $true
+}
+
+New-Item @NewItemParams
+
+$NewItemParams = @{
+    Path     = ~/.gitconfig-ple103
+    ItemType = "SymbolicLink"
+    Value    = "$PSScriptRoot\dotfiles\git\.gitconfig-ple103"
+    Force    = $true
+}
+
+New-Item @NewItemParams
+
+$NewItemParams = @{
+    Path     = ~/.gitconfig-work
+    ItemType = "SymbolicLink"
+    Value    = "$PSScriptRoot\dotfiles\git\.gitconfig-work"
+    Force    = $true
+}
+
+New-Item @NewItemParams
